ca21912f9b1a3f620b4cf6d0b0d48b9b822c2d95
JB <jb.champetier@gmail.com>
2024-04-17 16:50:15 +0200

    last commit

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,28 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { OlympicCountry } from '../models/Olympic';
+import * as fs from 'fs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
+
   private olympicUrl = './assets/mock/olympic.json';
   private olympics$ = new BehaviorSubject<OlympicCountry[]>([]);
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
     return this.http.get<OlympicCountry[]>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next([]);
-        return caught;
-      })
+        tap((value) => this.olympics$.next(value)),
+        catchError((error) => {
+            console.error('Error loading initial data:', error);
+            // Inform the user
+            alert("Une erreur est survenue lors du chargement des données.");
+            this.addErrorToLogFile(error);
+            return of([]);
+        })
     );
+  }
+  addErrorToLogFile(error: Error) {
+    const logFilePath: string = "./assets/LogFile.txt";
+    const errorMessage: string = `${new Date().toISOString()} : ${error.message}\n`;
+    fs.appendFile(logFilePath,errorMessage,(err: any) =>{
+      if (err) {
+        console.error("Une erreur s'est produite lors de l'écriture dans le log file :", err);
+        return;
+    }
+    console.log("L'erreur a été ajoutée au log file");
+    });
   }
 
   getOlympics() {
@@ -35,3 +48,4 @@ export class OlympicService {
     );
   }
 }
+
